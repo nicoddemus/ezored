62755d9c09456d23a7b1a08ccc0b73e04b352b68
Paulo Coutinho <paulo@prsolucoes.com>
2017-11-29 17:15:12 -0200

    python version

--- ezored/models/repository.py
+++ ezored/models/repository.py
@@ -78,6 +78,8 @@ class Repository(object):
                 else:
                     Logger.f('Problems when obtain dependency: {0}'.format(self.rep_name))
 
+            # TODO: TERMINAR AQUI E O DEPOIS DE BAIXAR A FUNÇÃO DE EXECUTAR O BUILD UNICA PRA GITHUB E LOCAL
+
     def get_git_data(self):
         # it will return a tuple of 3 elements with this pattern
         # 1 = repository name
