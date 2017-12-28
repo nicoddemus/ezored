9c6772031913a8f174dc93e66214ff5bb35c9161
Paulo Coutinho <paulo@prsolucoes.com>
2017-12-28 21:44:43 -0200

    fix issue #1

--- tests/models/test_target.py
+++ tests/models/test_target.py
@@ -91,7 +91,7 @@ class TestTarget(TestCase):
             repository=repository
         )
 
-        project.dependencies.append(dependency)
+        project.dependencies = [dependency]
 
         # process data
         process_data = ProcessData()
@@ -101,17 +101,39 @@ class TestTarget(TestCase):
         # process target data
         for target in project.targets:
             # get all target data from project dependencies
+            print('Who is the target:')
             print(target)
             target_data = TargetData()
 
+            print('There is {0} dependencies'.format(len(project.dependencies)))
+
             for dependency in project.dependencies:
+                print(dependency.get_name())
                 dependency.prepare_from_process_data(process_data)
 
                 new_target_data = TargetData()
+
+                # show who is the object
+                print('Who is the objects:')
+                print(target_data)
+                print(new_target_data)
+
+                # show before data
+                print('Before data:')
+                print(target_data.c_flags)
+                print(new_target_data.c_flags)
+
                 new_target_data.c_flags.extend(['flag'])
+
+                # show current data
+                print('Current data:')
+                print(target_data.c_flags)
+                print(new_target_data.c_flags)
 
                 target_data.merge(new_target_data)
 
+                # show after merge data
+                print('After merge data:')
                 print(new_target_data.c_flags)
                 print(target_data.c_flags)
 
