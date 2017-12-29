540897b7cb072e1460a20733caaad6a97f24cb3a
Paulo Coutinho <paulo@prsolucoes.com>
2017-12-28 22:48:02 -0200

    fix issue #1

--- tests/models/test_target.py
+++ tests/models/test_target.py
@@ -121,9 +121,6 @@ class TestTarget(TestCase):
 
             target_data = TargetData()
 
-            print('There is {0} dependencies'.format(len(project.dependencies)))
-            print("")
-
             for dependency in project.dependencies:
                 print('Who is the dependency:')
                 print(dependency.get_name())
@@ -132,34 +129,7 @@ class TestTarget(TestCase):
                 dependency.prepare_from_process_data(process_data)
 
                 new_target_data = TargetData()
-
-                # show who is the object
-                print('Who is the objects:')
-                print(target_data)
-                print(new_target_data)
-                print("")
-
-                # show before data
-                print('Before data:')
-                print(target_data.c_flags)
-                print(new_target_data.c_flags)
-                print("")
-
                 new_target_data.c_flags.extend(['flag'])
-
-                # show current data
-                print('Current data:')
-                print(target_data.c_flags)
-                print(new_target_data.c_flags)
-                print("")
-
                 target_data.merge(new_target_data)
-                print("")
-
-                # show after merge data
-                print('After merge data:')
-                print(new_target_data.c_flags)
-                print(target_data.c_flags)
-                print("")
 
             self.assertEqual(len(target_data.c_flags), 1)
