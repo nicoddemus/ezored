89f6699703f002a9b48fda6db95d5ab84c9bf111
Paulo Coutinho <paulo@prsolucoes.com>
2018-01-10 15:49:55 -0200

    issue #3

--- ezored/commands/target.py
+++ ezored/commands/target.py
@@ -93,6 +93,7 @@ class Target(Base):
 
                         for target_task_data in target_tasks_data:
                             task = Task.from_dict(target_task_data)
+                            task.parse(process_data)
                             target_data.tasks.append(task)
 
                     # run all tasks
